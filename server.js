0abc3ade508e5d48ef7fcaf2bcc07d94c5fbd662
shanima333 <75494918+shanima333@users.noreply.github.com>
2023-07-15 21:27:58 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
 const http = require('http');
 
-const hostname = 'localhost';
-const port = 3000;
-
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, World!\n');
+  res.end('Hello, world!');
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
